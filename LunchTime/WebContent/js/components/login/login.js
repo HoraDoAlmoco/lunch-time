70460068e12c304c51b8957b88caedcaa3fec0bd
hsalmeida <hsalmeida@gmail.com>
2015-05-07 16:54:30 -0300

    ajustes no modelo do projeto

---
+++ LunchTime/WebContent/js/components/login/login.js
@@ -0,0 +1,24 @@
+(function(){
+	var app = angular.module('login', [])
+	.controller('LoginController', 
+			['$scope', '$state', '$http', '$rootScope', 
+			 function ($scope, $state, $http, $rootScope) {
+		$scope.formData = {};
+		$scope.processLogin = function (){
+			//alert($scope.pass + " " + $scope.login);
+			
+			$http.get("/lunchtime/local-principal.json").success(function(data, status, headers, config) {
+				$rootScope.usuario = {};
+				$rootScope.usuario.grupoPrincipal = {};
+				$rootScope.usuario.grupoPrincipal = data;
+				
+				$state.go("mapa", {
+					'centerLat': $rootScope.usuario.grupoPrincipal.latitude,
+					'centerLng': $rootScope.usuario.grupoPrincipal.longitude,
+					'nomePrincipal' : $rootScope.usuario.grupoPrincipal.nome
+				});
+
+			});
+		}
+	}])
+})();
---
+++ LunchTime/WebContent/js/components/user/user.js
@@ -0,0 +1,22 @@
+(function(){
+	var app = angular.module('user', [])
+	.controller('UserController', 
+			['$scope', '$state', '$stateParams', '$document', '$rootScope', 
+	        function($scope, $state, $stateParams, $document, $rootScope){
+		$scope.title = "Meus dados";
+		$scope.coreToolBar = "blue";
+		
+		$scope.name = "Nome e Sobrenome";
+		$scope.login = "login para login :P";
+		$scope.email = "email@emailqualquer.com";
+		
+		$scope.backtomap = function (){
+			$state.go("mapa", {
+				'centerLat': $rootScope.usuario.grupoPrincipal.latitude,
+				'centerLng': $rootScope.usuario.grupoPrincipal.longitude,
+				'nomePrincipal' : $rootScope.usuario.grupoPrincipal.nome
+			});
+		}
+		
+	}])
+})();
